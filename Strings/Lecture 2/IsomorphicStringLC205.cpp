ae1b040785d0478d8833fde95c6d730c4c4ebd80
Harshita28m <harshitamarwaha111o@gmail.com>
2024-03-18 00:53:31 +0530

    isomorphic string

--- Strings/Lecture 2/IsomorphicStringLC205.cpp
+++ Strings/Lecture 2/IsomorphicStringLC205.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 int main() {
-    string s="physicswallah";
-    string t="wallahphysics";
-    sort(s.begin(),s.end());
-    sort(t.begin(),t.end());
-    if(s==t) cout<<"yes";
-    else cout<<"no";
-   
+    string s="egg";
+    string t="add";
+        vector <int> v(150,1000);
+        if(s.size() != t.size()) return false;
+       for(int i=0;i<s.size();i++){
+        int idx = (int)s[i];
+        if(v[idx]==1000) v[idx] = s[i]-t[i];
+        else if (v[idx]!=s[i]-t[i]) cout<<"false";
+       }
+       for (int i=0;i<150;i++){
+        v[i]=1000;
+       }
+for(int i=0;i<s.size();i++){
+        int idx = (int)t[i];
+        if(v[idx]==1000) v[idx] = t[i]-s[i];
+        else if (v[idx]!=t[i]-s[i]) cout<<"false";
+       }
+       cout<<"true";
 
     return 0;
 }
