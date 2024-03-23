b4ac8af3aad68f216c9744e46caf612ee1643a71
Harshita28m <harshitamarwaha111o@gmail.com>
2024-03-24 00:28:57 +0530

    done

--- Strings/Leetcode Questions/SortCharactersbyfrequencyLC451.cpp
+++ Strings/Leetcode Questions/SortCharactersbyfrequencyLC451.cpp
@@ -1,33 +1,32 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 
-string frequencySort(string s) {
-        vector<int> v(256, 0);
-        for (char c : s) {
-            int idx = int(c);
-            v[idx]++;
-        }
-        
-        
-        vector<pair<int, char>> freq_chars;
-        for (int i = 0; i < 256; i++) {
-            if (v[i] > 0) {
-                freq_chars.push_back({v[i], char(i)});
-            }
-        }
-        
-        
-        sort(freq_chars.rbegin(), freq_chars.rend());
-        
-        
-        string ans = "";
-        for (auto& p : freq_chars) {
-            ans += string(p.first, p.second);
-        }
-        
-        return ans;
+using namespace std;
+
+int main() {
+    string s ="tree";
+    vector<int> v(150, 0);
+    for (int i = 0; i < s.size(); i++) {
+        int idx = int(s[i]);
+        v[idx]++;
     }
-int main(){
+
+    vector<pair<int, char>> freq_chars;
+    for (int i = 0; i < 150; i++) {
+        if (v[i] > 0) {
+            freq_chars.push_back({v[i], char(i)});
+        }
+    }
+    sort(freq_chars.rbegin(), freq_chars.rend());
+
+    string ans = "";
+    for (auto& p : freq_chars) {
+        ans += string(p.first, p.second);
+    }
+
+    cout << ans << endl;
     
+    return 0;
 }
