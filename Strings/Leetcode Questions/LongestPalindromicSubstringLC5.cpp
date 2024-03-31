f60e5b6750621cea14bd6af6ea3e49acd87e341d
Harshita28m <harshitamarwaha111o@gmail.com>
2024-04-01 02:29:16 +0530

    done

--- Strings/Leetcode Questions/LongestPalindromicSubstringLC5.cpp
+++ Strings/Leetcode Questions/LongestPalindromicSubstringLC5.cpp
@@ -3,21 +3,23 @@ using namespace std;
 
 int main() {
     
-string ans="babad";
-        if(s.length() == 1) return s;
-        for (int i=0;i<s.size()-1;i++){
+string s="babad";
+string ans="";
+         
+        if(s.length() == 1) cout<<s;
+        else{
+            for (int i=0;i<s.size()-1;i++){
             for(int j=i;j<s.size();j++){
             string str = s.substr(i, j - i + 1);
             string test = str;
             reverse(str.begin(), str.end());
-            if(test == str){
-                if(test.length()>ans.length()){
-                    ans= test;
-                }
-                
-            }
+            if(test == str && test.length()>ans.length()){
+        ans= test;
+        }
         }
     }
+    }
+    
    cout<<ans;
     return 0;
 }
