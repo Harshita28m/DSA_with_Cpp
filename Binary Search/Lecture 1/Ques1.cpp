99af996002b840319d73c03857c9b9228b1c3041
Harshita28m <harshitamarwaha111o@gmail.com>
2024-06-21 22:53:44 +0530

    binary search

--- Binary Search/Lecture 1/Ques1.cpp
+++ Binary Search/Lecture 1/Ques1.cpp
@@ -4,19 +4,20 @@ int lowerBound(vector<int> arr, int n, int x) {
 	int low =0;
 	int high =n-1;
 	int mid=low+(high-low)/2;
-	while(low<high){
-		if(x==arr[mid]) return arr[mid-1];
+	while(low<=high){
+		if (x == arr[mid]) {
+            if (mid > 0) 
+                return arr[mid - 1];
+            else
+                return arr[mid];
+		}
 		else if(x<arr[mid]) high = mid-1;
-		else if (x>arr[mid]) low = mid+1;
-		else return arr[n+1];
-
-	}
+		else low = mid+1;
+		}
 	return arr[high];
 
 }
 int main(){
     vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
-    int n = arr.size();
-    int x = 8;
-    cout<<lowerBound(arr,n,x);
+    cout<<lowerBound(arr,10,8);
 }
