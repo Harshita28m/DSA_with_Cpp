4187b2edb61dea439f53f871ebf031cdc3b8fffd
Harshita28m <harshitamarwaha111o@gmail.com>
2024-06-23 12:15:44 +0530

    lower bound

---
+++ Binary Search/Lecture 1/LowerBound.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lowerBound(vector<int> arr, int n, int x) {
+	int low =0;
+	int high =n-1;
+	int flag =0;
+	while(low<=high){
+		int mid=low+(high-low)/2;
+		if (x == arr[mid]){ 
+		flag =1;
+		return arr[mid-1];
+		break;
+		}
+		else if(x>arr[mid]) low = mid+1;
+		else high = mid-1;
+		}
+	if(flag==0) return arr[high];
+
+}
+int main(){
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,12};
+    cout<<lowerBound(arr,10,11);
+}
