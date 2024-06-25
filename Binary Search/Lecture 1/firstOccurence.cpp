d0a8533bbb0b1246e3aa8ca1cfbef601a21a41fd
Harshita28m <harshitamarwaha111o@gmail.com>
2024-06-25 16:53:20 +0530

    first occurence

---
+++ Binary Search/Lecture 1/firstOccurence.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int firstOccurence(vector<int> arr, int n, int x) {
+	int low =0;
+	int high =n-1;
+	while(low<=high){
+		int mid=low+(high-low)/2;
+		if (x == arr[mid]){ 
+            if(arr[mid-1]==x){
+                high = mid-1;
+            }
+        else{
+		return mid;
+		break;
+        }
+		}
+		else if(x>arr[mid]) low = mid+1;
+		else high = mid-1;
+		}
+	return -1;
+
+}
+int main(){
+    vector<int> arr = {1,2,2,2,3,4,5,6,7,8,9,12};
+    cout<<firstOccurence(arr,12,2);
+}
