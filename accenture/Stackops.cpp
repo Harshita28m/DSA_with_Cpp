6644a4d21d9583585427eb064f28421baf1c57ac
Harshita28m <harshitamarwaha111o@gmail.com>
2024-09-04 17:56:10 +0530

    questions

--- accenture/Stackops.cpp
+++ accenture/Stackops.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 int main(){
     stack<int> s;
+    stack<int> st;
     s.push(1);
     s.push(6);
     s.push(8);
 while(s.size()>0){
-cout<<s.top()<<" ";
+st.push(s.top());
 s.pop();
+}
+while(st.size()>0){
+cout<<st.top()<<" ";
+st.pop();
 }
 }
